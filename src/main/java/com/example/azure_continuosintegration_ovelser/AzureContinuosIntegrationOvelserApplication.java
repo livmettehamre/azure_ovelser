23586963d213ee045d2472224119cd1421ac3b6e
livmettehamre <91458843+livmettehamre@users.noreply.github.com>
2021-11-12 13:02:08 +0100

    Opprettet user-agent

--- src/main/java/com/example/azure_continuosintegration_ovelser/AzureContinuosIntegrationOvelserApplication.java
+++ src/main/java/com/example/azure_continuosintegration_ovelser/AzureContinuosIntegrationOvelserApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @SpringBootApplication
 @RestController
 public class AzureContinuosIntegrationOvelserApplication {
@@ -17,6 +19,11 @@ public class AzureContinuosIntegrationOvelserApplication {
     @GetMapping("/")
     public String helloWorld(@RequestParam(defaultValue = "Cloud") String name) {
         return "Hello " + name;
+    }
+
+    @GetMapping("/user-agent")
+    public String userAgent(HttpServletRequest request) {
+        return request.getHeader("User-Agent");
     }
 
 }
